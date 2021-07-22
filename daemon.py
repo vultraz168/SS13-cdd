d18f743270562048c64de8cae60c1db8c4481e7c
Rainbow Llama <rainbowllama123@airmail.cc>
2021-07-21 18:45:49 -0700

    update to daemon.py

--- daemon.py
+++ daemon.py
@@ -1,1 +1,32 @@
-# Yes!
+import subprocess
+
+# CHANGE BELOW
+
+PATH_TO_SS13 = "D:\mlpstation13"
+#BRANCH = "Bleeding-Edge"
+BRANCH = "main"
+
+# ------------
+
+def init():
+    #os.chdir(PATH_TO_SS13) # UNCOMMENT THIS IN THE FINAL SCRIPT
+
+# Check for new content on GitHub
+def get_remote():
+    subprocess.run(["git fetch"], shell=True)
+
+    # Check for merge conflicts
+    merge_check = subprocess.run(["git merge --no-commit --no-ff "+BRANCH], shell=True)
+    subprocess.run(["git merge --abort"], shell=True)
+
+    # Exit status is 0 if the merge is possible.
+    # Rebase - don't use a merge commit.
+    if (merge_check.returncode == 0):
+        subprocess.run(["git rebase main"], shell=True)
+
+    # check for divergence
+
+    #print(up.stdout)
+    pass
+
+get_remote()
